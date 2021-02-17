b568c998944457a27a7064dc0aa464db19385976
alliecatstanz34 <alliecatstanz34@gmail.com>
2021-02-16 20:16:48 -0800

    added more attributes to relation model, not yet finalized

--- relation_model/relation_model.sql
+++ relation_model/relation_model.sql
@@ -21,7 +21,7 @@ CREATE TABLE UserRating(
     userID VARCHAR,
     funny INTEGER,
     cool INTEGER,
-    usefull INTEGER,
+    useful INTEGER,
     PRIMARY KEY (userID),
     FOREIGN KEY (userID) REFERENCES User (userID)
 );
@@ -29,7 +29,7 @@ CREATE TABLE UserRating(
 CREATE TABLE FriendsWith(
     userID VARCHAR,
     friendID VARCHAR,
-    PRIMARY KEY (userID),
+    PRIMARY KEY (userID,friendID),
     FOREIGN KEY (userID,friendID) REFERENCES User (userID)
 );
 
@@ -43,7 +43,7 @@ CREATE TABLE FansUser(
 CREATE TABLE LikesUser(
     likerID VARCHAR, --ID of user liking other user
     userID VARCHAR, --ID of user being liked
-    -- reason, 
+    reason VARCHAR, -- cool funny or useful
     PRIMARY KEY (likerID, userID),
     FOREIGN KEY (userID, likerID) REFERENCES User (userID)
 );
@@ -63,7 +63,14 @@ CREATE TABLE Buisness(
     stars REAL,
     tipCount INTEGER,
     isOpen BOOLEAN,
-    attributes VARCHAR, --list of attribute key words, I *think* this will be cleaner looking rather than a more tables for each individual attribute.
+    wifi BOOLEAN,
+    acceptsCreditCards BOOLEAN,
+    goodForKids BOOLEAN,
+    wheelchairAccessible BOOLEAN,
+    dogsAllowed BOOLEAN,
+    priceRange INTEGER, --1 through 4
+    parking VARCHAR, --include bike parking
+    catagories VARCHAR,
     PRIMARY KEY (buisnessID)
     CHECK (stars>=0, tipCount>=0)
 );
@@ -77,8 +84,47 @@ CREATE TABLE Restaurant(
     dinner BOOLEAN,
     dessert BOOLEAN,
     lateNight BOOLEAN,
-    --might lump meal times into one list like attributes and ambiance 
-    ambiance VARCHAR, --list of ambiances 
+    goodForGroups BOOLEAN,
+    reservations BOOLEAN,
+    outdoorSeating BOOLEAN, 
+    hasTV BOOLEAN,
+    noiseLevel VARCHAR,
+    alcohol VARCHAR,
+    catering BOOLEAN,
+    delivery BOOLEAN,
+    takeout BOOLEAN,
+    PRIMARY KEY (buisnessID),
+    FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
+);
+
+CREATE TABLE RestaurantAmbiance(
+    buisnessID VARCHAR,
+    romantic BOOLEAN,
+    intimate BOOLEAN,
+    classy BOOLEAN,
+    hipster BOOLEAN,
+    divey BOOLEAN,
+    touristy BOOLEAN,
+    trendy BOOLEAN,
+    upscale BOOLEAN,
+    casual BOOLEAN,
+    PRIMARY KEY (buisnessID),
+    FOREIGN KEY (buisnessID) REFERENCES Restaurant (buisnessID)
+);
+
+-- includes salons, gyms, hotels, pharmacies, dry cleaners, hospitals/clinics, atms, banks, mail/shipping
+CREATE TABLE PersonalService(
+    buisnessID VARCHAR,
+    appointmentOnly BOOLEAN,
+    acceptsInsurance BOOLEAN,
+    PRIMARY KEY (buisnessID),
+    FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
+);
+
+-- parks, music, museums, attractions, libraries, movie theaters 
+CREATE TABLE Entertainment(
+    buisnessID VARCHAR,
+    appointmentOnly BOOLEAN,
     PRIMARY KEY (buisnessID),
     FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
 );
@@ -91,6 +137,7 @@ CREATE TABLE BuisnessAddress(
     PRIMARY KEY (buisnessID),
     FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
 );
+
 
 CREATE TABLE BuisnessLocation(
     buisnessID VARCHAR,
@@ -109,18 +156,6 @@ CREATE TABLE BuisnessHours(
     thHrs VARCHAR,
     fHrs VARCHAR,
     sHrs VARCHAR,
-    PRIMARY KEY (buisnessID),
-    FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
-);
-
-CREATE TABLE BuisnessParking(
-    buisnessID VARCHAR,
-    garage BOOLEAN,
-    street BOOLEAN,
-    validated BOOLEAN,
-    lot BOOLEAN,
-    valet BOOLEAN,
-    bikeParking BOOLEAN,
     PRIMARY KEY (buisnessID),
     FOREIGN KEY (buisnessID) REFERENCES Buisness (buisnessID)
 );
