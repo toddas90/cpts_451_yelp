ab5a0ca13f3be3074f46557f8da74e41a636e620
alliecatstanz34 <alliecatstanz34@gmail.com>
2021-03-24 18:08:14 -0700

    tested updates and triggers, working version

--- docs/bestteam_TRIGGERS.sql
+++ docs/bestteam_TRIGGERS.sql
@@ -1,106 +1,77 @@
--- Update AND Trigger for up-to-date number of checkins in the Business table 
-CREATE OR REPLACE FUNCTION UpdateNumCheckins() RETURNS trigger AS $$ 
+-- Function to maintain the checkInCount for each businessID in Business
+CREATE OR REPLACE FUNCTION UpdateCheckInCount() RETURNS trigger AS $$ 
 BEGIN 
-    IF (Business.businessID == NEW.businessID) THEN 
-        NEW.CheckInCount = Business.CheckInCount + 1;
-    END IF;
-    RETURN NEW;
+   UPDATE  Business   
+   SET  checkInCount = checkInCount+1   
+   WHERE Business.businessID=NEW.businessID;   
+   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Update for up-to-date number of tips written about a business in Business table 
--- CREATE OR REPLACE FUNCTION UpdateNumTips() RETURNS trigger AS $$
--- BEGIN 
---     IF (Business.businessID == NEW.businessID) THEN 
---         NEW.NumTips = Business.NumTips + 1;
---     END IF;
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- Update for up-to-date total number of likes in the Users table 
--- CREATE OR REPLACE FUNCTION UpdateTotalLikes() RETURNS trigger AS $$
--- BEGIN 
---     IF (Users.userID == NEW.userID) THEN 
---         NEW.totalLikes = Users.totalLikes + NEW.likes;
---     END IF;
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- -- Update for up-to-date number of tips written by a user in the Users table 
--- CREATE OR REPLACE FUNCTION UpdateTipCount() RETURNS trigger AS $$
--- BEGIN 
---     IF (Users.userID == NEW.userID) THEN 
---         NEW.tipCount = Users.tipCount + 1;
---     END IF;
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
 
 -- Function combining UpdateNumTips, UpdateTotalLikes, and UpdateTipCount to be ran with the same trigger
 CREATE OR REPLACE FUNCTION UpdateTipInfo() RETURNS trigger AS $$ 
 BEGIN 
-    IF (Users.userID == NEW.userID) THEN 
-        NEW.tipCount = Users.tipCount + 1;
-        NEW.totalLikes = Users.totalLikes + NEW.likes;
-    END IF;
-    IF (Business.businessID == NEW.businessID) THEN 
-        NEW.NumTips = Business.NumTips + 1;
-    END IF;
+    UPDATE Users
+    SET totalLikes = totalLikes + NEW.likes, tipCount = tipCount + 1
+    WHERE Users.userID = NEW.userID;
+    UPDATE Business
+    SET tipCount = tipCount + 1
+    WHERE Business.businessID = NEW.businessID;
+    RETURN NEW;
+
 END;
 $$ LANGUAGE plpgsql;
 
 -- Trigger for updating tipCount in Business and Users, and totalLikes in Users
 CREATE TRIGGER NewTip
-AFTER
-INSERT ON Tip FOR EACH STATEMENT EXECUTE FUNCTION UpdateTipInfo();
+AFTER INSERT ON Tip 
+FOR EACH ROW 
+EXECUTE FUNCTION UpdateTipInfo();
 
+-- Trigger for updating checkInCount in Business
 CREATE TRIGGER NewCheckIn
-AFTER
-INSERT ON ChecksIn FOR EACH STATEMENT EXECUTE FUNCTION UpdateNumCheckins();
+AFTER INSERT ON ChecksIn 
+FOR EACH ROW 
+EXECUTE FUNCTION UpdateCheckInCount();
 
--- Test code for NewCheckIn Trigger
-SELECT businessID,
-    checkInCount
-FROM Business
-WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
-INSERT INTO ChecksIn
-VALUES ('gnKjwL_1w79qoiV3IC_xQQ', '2021-03-23 12:00:00');
-SELECT businessID,
-    checkInCount
-FROM Business
-WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
--- clean 
-DROP TRIGGER IF EXISTS NewCheckIn on ChecksIn;
--- Test code for NewTip Trigger
-SELECT businessID,
-    tipCount
-FROM Business
-WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
-SELECT userID,
-    tipCount,
-    totalLikes
-FROM Users
-WHERE userID = 'jRyO2V1pA4CdVVqCIOPc1Q';
-INSERT INTO Tip
-VALUES (
-        'jRyO2V1pA4CdVVqCIOPc1Q',
-        'gnKjwL_1w79qoiV3IC_xQQ',
-        '2021-03-23 12:00:00',
-        '10',
-        'Good Atmosphere!'
-    );
-SELECT businessID,
-    tipCount
-FROM Business
-WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
-SELECT userID,
-    tipCount,
-    totalLikes
-FROM Users
-WHERE userID = 'jRyO2V1pA4CdVVqCIOPc1Q';
--- clean 
--- DROP TRIGGER IF EXISTS NewTip on Business;
--- DROP TRIGGER IF EXISTS NewTip on Users;
-DROP TRIGGER IF EXISTS NewTip on Tip;
+-- -- Test code for NewCheckIn Trigger
+-- SELECT businessID,
+--     checkInCount
+-- FROM Business
+-- WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
+-- INSERT INTO ChecksIn
+-- VALUES ('gnKjwL_1w79qoiV3IC_xQQ', '2021-03-23 12:00:00');
+-- SELECT businessID,
+--     checkInCount
+-- FROM Business
+-- WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
+-- -- clean 
+-- DROP TRIGGER IF EXISTS NewCheckIn on ChecksIn;
+-- DROP FUNCTION IF EXISTS UpdateCheckInCount;
+
+-- DELETE FROM ChecksIn 
+-- WHERE businessID = 'gnKjwL_1w79qoiV3IC_xQQ' AND checkInDate = '2021-03-23 12:00:00';
+
+-- -- Test code for NewTip Trigger
+-- SELECT businessID, tipCount
+-- FROM Business
+-- WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
+-- SELECT userID, tipCount, totalLikes
+-- FROM Users
+-- WHERE userID = 'jRyO2V1pA4CdVVqCIOPc1Q';
+-- INSERT INTO Tip
+-- VALUES ('jRyO2V1pA4CdVVqCIOPc1Q', 'gnKjwL_1w79qoiV3IC_xQQ', '2021-03-23 12:00:00', '10', 'Good Atmosphere!');
+-- SELECT businessID, tipCount
+-- FROM Business
+-- WHERE BusinessID = 'gnKjwL_1w79qoiV3IC_xQQ';
+-- SELECT userID, tipCount, totalLikes
+-- FROM Users
+-- WHERE userID = 'jRyO2V1pA4CdVVqCIOPc1Q';
+
+-- -- clean 
+-- DROP TRIGGER IF EXISTS NewTip on Tip;
+-- DROP FUNCTION IF EXISTS UpdateTipInfo;
+
+-- DELETE FROM Tip 
+-- WHERE userID = 'jRyO2V1pA4CdVVqCIOPc1Q' AND businessID = 'gnKjwL_1w79qoiV3IC_xQQ' AND dateWritten = '2021-03-23 12:00:00';
