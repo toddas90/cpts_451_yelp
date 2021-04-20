999f116a8e33efac01b2f65ce2208ce75139d157
Andrew Todd <andrew123ast@outlook.com>
2021-04-20 15:52:00 -0700

    Commented the businessForm.cs file

--- src/cpts_451_yelp/BusinessForm.cs
+++ src/cpts_451_yelp/BusinessForm.cs
@@ -15,30 +15,38 @@ namespace cpts_451_yelp
             Text = "Add Tip"
         };
 
+        // These hold the number of businesses in the state/city
         private string statenum = "";
         private string citynum = "";
 
+        // Makes sure the tip isn't a duplicate
         private bool isDuplicate;
 
+        // Local copy of the user and business
         private UserInfo user;
         private Business bus;
 
+        // shared info
         SharedInfo s = new SharedInfo();
 
+        // Textbox for adding tips
         TextBox newTip = new TextBox
         {
             PlaceholderText = "New Tip",
             Size = new Size(800, -1)
         };
 
+        // data store for tips
         DataStoreCollection<TipInfo> data = new DataStoreCollection<TipInfo>();
 
+        // Grid to display tips
         GridView grid = new GridView<TipInfo>
         {
             AllowMultipleSelection = true,
             AllowEmptySelection = true
         };
 
+        // Event handler for clicks
         public event EventHandler<EventArgs> Click;
 
         // Main entry point for business window.
@@ -58,21 +66,27 @@ namespace cpts_451_yelp
             addColGrid();
             this.Content = layout; // Instantiates the layout
 
+            // Add tip button event
             addTip.Click += new EventHandler<EventArgs>(addTipHelper);
         }
 
         // Queries for loading the number of businesses.
         private void loadBusinessNums()
         {
+            // Gets number of businesses in the state
             string sqlStr1 = "SELECT count(*) from businessaddress WHERE businessstate = (SELECT businessstate from businessaddress WHERE businessid = '" + bus.bid + "');";
             s.executeQuery(sqlStr1, loadBusinessNumsStateHelper, true);
+
+            // Gets number of businesses in the city
             string sqlStr2 = "SELECT count(*) from businessaddress WHERE businesscity = (SELECT businesscity from businessaddress WHERE businessid = '" + bus.bid + "');";
             s.executeQuery(sqlStr2, loadBusinessNumsCityHelper, true);
         }
 
+        // Loads the tips into the grid
         private void loadBusinessTipsHelper()
         {
             data.Clear();
+            // Query for the tips
             string sqlStr = "SELECT dateWritten, userName, likes, textWritten FROM Tip, Users WHERE Users.userID = Tip.userID AND businessID = '" + bus.bid + "' ORDER BY dateWritten;";
             s.executeQuery(sqlStr, loadBusinessTipsHelper, true);
             grid.DataStore = data;
@@ -90,6 +104,7 @@ namespace cpts_451_yelp
             citynum = R.GetInt32(0).ToString();
         }
 
+        // Puts the tip info into a tip class to keep the info together
         private void loadBusinessTipsHelper(NpgsqlDataReader R)
         {
             data.Add(new TipInfo()
@@ -101,34 +116,37 @@ namespace cpts_451_yelp
             });
         }
 
+        // Adds the tips to the business!
         private void addTipHelper(object sender, EventArgs e)
         {
+            // Query to see if the tip exists
             String check = @"SELECT userID, businessID, textWritten FROM Tip WHERE userID = '" + user.UserID + @"' 
             AND businessid = '" + bus.bid + "' AND textWritten = '" + newTip.Text.ToString() + "';";
             s.executeQuery(check, tipExists, true);
-            if (user.UserID != null && !isDuplicate && newTip.Text.Length > 0)
+            if (user.UserID != null && !isDuplicate && newTip.Text.Length > 0) // Makes sure theres a real tip
             {
-
+                // Query to insert the tip
                 string cmd = @"INSERT INTO Tip (userid, businessID, dateWritten, likes, textWritten)
                     VALUES ('" + user.UserID + "', '" + bus.bid + "', '" +
                     DateTime.Now + "', 0,'" + newTip.Text.ToString() + "') ;";
                 s.executeQuery(cmd, empty, false);
                 loadBusinessTipsHelper();
             }
-            else if (newTip.Text.Length == 0)
+            else if (newTip.Text.Length == 0) // If it can't insert
             {
                 MessageBox.Show("Cannot submit an empty tip!");
             }
-            else if (isDuplicate)
+            else if (isDuplicate) // etc
             {
                 MessageBox.Show("That tip already exists!");
             }
-            else
+            else // etc
             {
                 MessageBox.Show("You must log in before you submit a tip!");
             }
         }
 
+        // Checks to see if the tip exists
         private void tipExists(NpgsqlDataReader R)
         {
             if (R.HasRows)
@@ -141,11 +159,15 @@ namespace cpts_451_yelp
             }
         }
 
+        // The execute reader method requires something to handle
+        // reading in the data. In this case we just wanted to insert.
+        // So, there is an empty method lol.
         private void empty(NpgsqlDataReader R)
         {
-
+            // Maybe add an easter egg?
         }
 
+        // Required by law to be here!!! (handles events)
         protected virtual void OnClick()
         {
             EventHandler<EventArgs> handler = Click;
@@ -202,6 +224,8 @@ namespace cpts_451_yelp
 
             layout.EndVertical();
         }
+
+        // Populates the grid with columns
         private void addColGrid()
         {
             grid.Columns.Add(new GridColumn
