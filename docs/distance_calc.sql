575f647f73387999d909938ea11441b2a8183c07
Christopher Nathman <nathmanc@gmail.com>
2021-04-21 18:10:41 -0700

    fixed distance function syntax errors

--- docs/distance_calc.sql
+++ docs/distance_calc.sql
@@ -1,18 +1,19 @@
 -- returns the distance between the two points in miles
-CREATE FUNCTION getDistance(@lat1 FLOAT, @lon1 FLOAT, @lat2 FLOAT, @lon2 FLOAT)
-RETURNS FLOAT
-AS
+CREATE OR REPLACE FUNCTION getDistance(lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT)
+RETURNS FLOAT AS $$
+DECLARE
+	lat1Rad FLOAT = RADIANS(lat1);
+	lon1Rad FLOAT = RADIANS(lon1);
+	lat2Rad FLOAT = RADIANS(lat2);
+	lon2Rad FLOAT = RADIANS(lon2);
+
+	earthRadius FLOAT = 3958.8;  -- radius of the earth in miles
+
+	haversine FLOAT = POWER(SIN((lat2Rad - lat1Rad) / 2), 2) + (COS(lat1Rad) * COS(lat2Rad) * POWER(SIN((lon2Rad - lon1Rad) / 2), 2)); -- haversine formula
+
+	distance FLOAT = 2 * earthRadius * ASIN(SQRT(haversine));  -- distance in miles
+
 BEGIN
-	DECLARE @lat1Rad FLOAT = RADIANS(lat1);
-	DECLARE @lon1Rad FLOAT = RADIANS(lon1);
-	DECLARE @lat2Rad FLOAT = RADIANS(lat2);
-	DECLARE @lon2Rad FLOAT = RADIANS(lon2);
-
-	DECLARE @earthRadius FLOAT = 3958.8;  -- radius of the earth in miles
-
-	DECLARE @haversine FLOAT = SQUARE(SIN((@lat2Rad - lat1Rad) / 2)) + (COS(@lat1Rad) * COS(@lat2Rad) * SQUARE(SIN((@lon2Rad - @lon1Rad) / 2))); -- haversine formula
-
-	DECLARE @distance FLOAT = 2 * @earthRadius * ASIN(SQRT(@haversine));  -- distance in miles
-	
-	RETURN @distance;
+	RETURN distance;
 END;
+$$ LANGUAGE plpgsql;
