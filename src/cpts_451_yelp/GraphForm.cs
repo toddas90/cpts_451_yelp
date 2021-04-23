4f71ca07a1f481c7111e46189beae41e80291324
Christopher Nathman <nathmanc@gmail.com>
2021-04-22 17:50:30 -0700

    completed functional bar graph when clicking user button

--- src/cpts_451_yelp/GraphForm.cs
+++ src/cpts_451_yelp/GraphForm.cs
@@ -12,73 +12,50 @@ namespace cpts_451_yelp
         // Main entry point for graph window.
         public GraphForm()
         {
+            MinimumSize = new Size(600, 400);
             createUI();
-        }
-
-        private Random rand = new Random(0);
-        private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
-        {
-            // return an array of difting random numbers
-            double[] values = new double[points];
-            values[0] = start;
-            for (int i = 1; i < points; i++)
-                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
-            return values;
+            this.Content = layout;
         }
 
         public void createUI()
         {
-            layout.DefaultSpacing = new Size(5, 5);
-            layout.Padding = new Padding(10, 10, 10, 10);
-
             // generate some random Y data
-            int pointCount = 5;
-            double[] ys1 = RandomWalk(pointCount);
-            double[] ys2 = RandomWalk(pointCount);
+            double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
 
             // create a series of bars and populate them with data
-            var seriesA = new OxyPlot.Series.ColumnSeries()
+            var series = new OxyPlot.Series.ColumnSeries()
             {
-                Title = "Series A",
-                StrokeColor = OxyPlot.OxyColors.Black,
-                FillColor = OxyPlot.OxyColors.Red,
-                StrokeThickness = 1
-            };
-
-            var seriesB = new OxyPlot.Series.ColumnSeries()
-            {
-                Title = "Series B",
+                Title = "Data",
                 StrokeColor = OxyPlot.OxyColors.Black,
                 FillColor = OxyPlot.OxyColors.Blue,
                 StrokeThickness = 1
             };
 
-            for (int i = 0; i < pointCount; i++)
+            for (int i = 0; i < 5; i++)
             {
-                seriesA.Items.Add(new OxyPlot.Series.ColumnItem(ys1[i], i));
-                seriesB.Items.Add(new OxyPlot.Series.ColumnItem(ys2[i], i));
+                series.Items.Add(new OxyPlot.Series.ColumnItem(data[i], i));
             }
 
             // create a model and add the bars into it
             var model = new OxyPlot.PlotModel
             {
-                Title = "Bar Graph (Column Series)"
+                Title = "Bar Graph"
             };
             model.Axes.Add(new OxyPlot.Axes.CategoryAxis());
-            model.Series.Add(seriesA);
-            model.Series.Add(seriesB);
+            model.Series.Add(series);
 
             // load the model into the user control
             Plot p = new Plot();
             p.Model = model;
+
+            layout.DefaultSpacing = new Size(5, 5);
+            layout.Padding = new Padding(10, 10, 10, 10);
 
             layout.BeginHorizontal();
             layout.BeginVertical();
             layout.Add(p);
             layout.EndHorizontal();
             layout.EndVertical();
-            //Eto.OxyPlot.Plot.IHandler.Model.set(model);
-            //plotView1.Model = model;
         }
 
     }
